0584d8fc7035f6ae955cd0bd124b3f7d6d054d7e
Diego Aldao <diegofrontdev@gmail.com>
2025-11-15 13:28:29 -0300

    añadido el javascript para las interacciones de la navegacion mobile y la seccion tablas

---
+++ app.js
@@ -0,0 +1,41 @@
+/*CAMBIO DE TABLAS*/
+const itemsTablas = document.querySelectorAll(".item-tabla");
+const imagenTabla = document.querySelector(".imagen-tabla");
+const nombreTabla = document.querySelector(".nombre-tabla-desk");
+
+const setCurrentTabla = (imagenSrc, nuevoNombre) => {
+  imagenTabla.src = imagenSrc;
+  nombreTabla.innerText = nuevoNombre;
+};
+
+itemsTablas.forEach((tabla) => {
+  tabla.addEventListener("click", () => {
+    setCurrentTabla(tabla.dataset.tabla, tabla.innerText);
+  });
+});
+
+/*TOGGLE MENU MOBILE*/
+const buttonMenuMobile = document.querySelector(".btn-nav-mobile");
+const buttonCloseMenuMobile = document.querySelector(".btn-cerrar-modal");
+const menuMobile = document.querySelector(".modal-nav-mobile");
+const body = document.querySelector("body");
+
+console.log(body);
+
+const toggleMenuMobile = (accion) => {
+  if (accion === "abrir") {
+    menuMobile.classList.add("modal-open");
+    body.style.overflow = "hidden";
+  } else {
+    menuMobile.classList.remove("modal-open");
+    body.style.overflow = "auto";
+  }
+};
+
+buttonMenuMobile.addEventListener("click", () => {
+  toggleMenuMobile("abrir");
+});
+
+buttonCloseMenuMobile.addEventListener("click", () => {
+  toggleMenuMobile("cerrar");
+});
